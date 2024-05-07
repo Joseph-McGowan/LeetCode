2e6b487a1e9bbc44e654b77237feb94511a93a4f
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-05-07 18:18:57 +0100

    anagram solution

--- Arrays & Hashing/Anagram.cpp
+++ Arrays & Hashing/Anagram.cpp
@@ -1,11 +1,36 @@
 //
 // Created by josep on 07/05/2024.
 //
+#include <algorithm>
 #include <iostream>
+#include <string>
+using std::string;
+
+class Solution {
+public:
+    static bool isAnagram(string s, string t) {
+
+        if(s.size() != t.size() )
+            return false;
+
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != t[i])
+                return false;
+        }
+        return true;
+    }
+
+};
+
 
 int main(int argc, char** argv) {
 
-    std::cout << "helloe" << std::endl;
+    //Solution a = new Solution;
+
+    std::cout << Solution::isAnagram("hel", "hello") << std::endl;
 
     return 1;
 }
