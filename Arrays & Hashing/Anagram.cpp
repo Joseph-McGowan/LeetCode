bf93fea48365a80e6aea40031a790c8b863b054a
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-05-07 17:57:58 +0100

    initial commit

---
+++ Arrays & Hashing/Anagram.cpp
@@ -0,0 +1,11 @@
+//
+// Created by josep on 07/05/2024.
+//
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+    std::cout << "helloe" << std::endl;
+
+    return 1;
+}
---
+++ Arrays & Hashing/test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by josep on 07/05/2024.
+//
+#include<iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "hello from test" << std::endl;
+}
