6558602513894f98a9eaf54e9c56614304673f0e
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-06-21 14:07:14 +0100

    Substring boilerplate

---
+++ slidingWindow/Substring.cpp
@@ -0,0 +1,30 @@
+//
+// Created by josep on 21/06/2024.
+//
+
+#include "string.h"
+#include "iostream"
+#include "hash_set"
+
+using namespace std;
+
+class Solution {
+public:
+   static int lengthOfLongestSubstring(string s) {
+        //set containg current chars of substring
+        //current length variable
+        //if new substring longer, = longest
+        //if repeat counter found, reset counter, char set
+        //return longest substring
+
+    }
+};
+
+
+
+int main() {
+
+    string a = "xyzxyzxyzz";
+    cout << Solution::lengthOfLongestSubstring(a) << endl;
+
+}
