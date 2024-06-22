ff0d3e5efd51a6890d4bf2f58e18ff2f8aae4ab2
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-06-22 11:46:32 +0100

    Substring complete

--- slidingWindow/Substring.cpp
+++ slidingWindow/Substring.cpp
@@ -5,17 +5,49 @@
 #include "string.h"
 #include "iostream"
 #include "hash_set"
+#include "unordered_set"
 
 using namespace std;
 
 class Solution {
 public:
    static int lengthOfLongestSubstring(string s) {
-        //set containg current chars of substring
-        //current length variable
-        //if new substring longer, = longest
-        //if repeat counter found, reset counter, char set
-        //return longest substring
+
+       //set containg current chars of substring
+       //current length variable
+       //if new substring longer, = longest
+       //if repeat counter found, reset counter, char set
+       //return longest substring
+
+
+
+       unordered_set<int> sbstrList;
+       int currentLength = 0;
+       int longest = 0;
+       int leftPointer = 0;
+       int rightPointer = 0;
+
+       for(int i = 0; i < s.length(); i++) {
+
+           if (!sbstrList.count(s[i])) {
+               sbstrList.emplace(s[i]);
+               currentLength++;
+               if (currentLength > longest) {
+                   longest = currentLength;
+               }
+           }
+           else {
+               while (s[leftPointer] != s[i]) {
+                    sbstrList.erase(s[leftPointer]);
+                   leftPointer++;
+                   currentLength--;
+               }
+               leftPointer++;
+           }
+       }
+
+       return longest;
+
 
     }
 };
@@ -24,7 +56,7 @@ public:
 
 int main() {
 
-    string a = "xyzxyzxyzz";
+    string a = "tmmzuxt";
     cout << Solution::lengthOfLongestSubstring(a) << endl;
 
 }
