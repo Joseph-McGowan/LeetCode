cf746beedad786855fe1bfc055fdb1c0b20c86ea
Joseph McGowan <josephdmcgowan68@gmail.com>
2024-06-24 19:20:10 +0100

    binSearch

---
+++ binSearch/binarySearch.cpp
@@ -0,0 +1,23 @@
+//
+// Created by joseph on 24/06/2024.
+//
+#include "iostream"
+#include "vector"
+
+
+using namespace std;
+
+
+class Solution {
+public:
+    static int search(vector<int>& nums, int target) {
+
+    }
+};
+
+
+int main() {
+    return 0;
+
+
+}
